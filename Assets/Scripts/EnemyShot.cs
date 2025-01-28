b111ce34e0fbe2022cfcc4041cc608bdab097a10
Yaren Bos <225040093@stu.istinye.edu.tr>
2025-01-28 21:35:00 +0300

    game

--- Assets/Scripts/EnemyShot.cs
+++ Assets/Scripts/EnemyShot.cs
@@ -2,25 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerShot : MonoBehaviour
+public class EnemyShot : MonoBehaviour
 {
-
     public float shotSpeed = 7f;
     public GameObject impactEffect;
-
-    public GameObject objectExplosion;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(shotSpeed * Time.deltaTime, 0f, 0f);
+        transform.position -= new Vector3(shotSpeed * Time.deltaTime, 0f, 0f);
 
     }
 
@@ -28,17 +25,10 @@ public class PlayerShot : MonoBehaviour
     {
         Instantiate(impactEffect, transform.position, transform.rotation);
 
-        if(other.tag == "Space Object")
+        if(other.tag == "Player")
         {
-            Instantiate(objectExplosion, other.transform.position, other.transform.rotation);
-            Destroy(other.gameObject);
+            HealthManager.instance.HurtPlayer();
         }
-
-        if(other.tag == "Enemy")
-        {
-            other.GetComponent<EnemyController>().HurtEnemy();
-        }
-
 
         Destroy(this.gameObject);
     }
