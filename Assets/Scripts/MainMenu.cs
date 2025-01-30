59601b7050b23db46ffb4212d987e6f5c8efdb07
Yaren Bos <225040093@stu.istinye.edu.tr>
2025-01-30 04:12:09 +0300

    game

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public string firstlevel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Leaving the game");
+    }
+
+    public void Credits()
+    {
+
+    }
+}
